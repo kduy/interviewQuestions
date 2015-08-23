27cca3c1440ae4fe9a56d92ee84ff67845973592
kduy <duyvkvn@gmail.com>
2015-08-23 19:28:07 +0200

    init dockerfile

--- StormFlinkPerformance/flink-app/src/main/java/com/nventdata/task/flink/Main.java
+++ StormFlinkPerformance/flink-app/src/main/java/com/nventdata/task/flink/Main.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.flink.streaming.connectors.kafka.flink;
+package com.nventdata.task.flink;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumReader;
@@ -47,7 +30,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class KafkaConsumerExample {
+public class Main {
 
 	private static String host;
 	private static int port;
@@ -117,8 +100,6 @@ public class KafkaConsumerExample {
 	}
     
     public static final class avroDecodingMap implements MapFunction<String, Tuple2<Integer, String>> {
-
-
         @Override
         public Tuple2<Integer,String> map(String value) throws Exception {
             Message message = new Message(value.getBytes());
