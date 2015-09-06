df3011c875def80c9444bce7595699eaedcb78be
kduy <duyvkvn@gmail.com>
2015-09-06 20:35:31 +0200

    [flink] done performance

--- StormFlinkPerformance/flink-app/src/main/java/com/nventdata/task/flink/performance/Performance.java
+++ StormFlinkPerformance/flink-app/src/main/java/com/nventdata/task/flink/performance/Performance.java
@@ -1,12 +1,12 @@
 package com.nventdata.task.flink.performance;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Measure performance metrics into csv file
@@ -17,7 +17,7 @@ public class Performance implements Serializable {
 
     /* name of application*/
     private String name;
-    
+
     /*store metrics into Lists*/
     private List<Long> timestamps;
     private List<Long> recordsVolList;
@@ -26,10 +26,10 @@ public class Performance implements Serializable {
     /*temporary store metrics*/
     private long buffer ;
     private long counter;
-    
+
     /*save metrics every 'interval' */
     private int interval;
-    
+
     /* write stored metrics to file every 'dumpInterval'*/
     private long dumpInterval = 0;
 
@@ -43,7 +43,7 @@ public class Performance implements Serializable {
 
     /*last moment at which metrics are stored to Lists*/
     protected  long lastAdd ;
-    
+
     private boolean firstWrite;
 
     public Performance (String name , int interval , int dumpInterval, String outputFolder){
@@ -64,10 +64,10 @@ public class Performance implements Serializable {
     /**
      * * Store measured data in 3 lists of timestamp, 
      *   number of records, and total volume of those records.
-     *   
+     *
      *   If time exceed the dumpInterval,
      *   print out all the list to file
-     *    
+     *
      * @param numOfRecords
      * @param recordsVolume
      * @param timestamp
@@ -96,7 +96,7 @@ public class Performance implements Serializable {
 
     /**
      * * Tracking new records
-     * 
+     *
      * *  store metrics into Lists
      * * 
      * @param numOfNewRecords
@@ -107,7 +107,7 @@ public class Performance implements Serializable {
         long dtime = ctime - lastAdd;
         buffer += newRecordsVolume;
         counter += numOfNewRecords;
-        
+
         // add metrics to Lists every 'interval'
         if (dtime > interval){
             lastAdd = ctime - (ctime % interval);
@@ -120,7 +120,7 @@ public class Performance implements Serializable {
         try {
             LOG.debug("Writing " + this.hashCode() + " thread:" +  Thread.currentThread().getId() + " CSV to output: " + outputFolder);
             FileWriter fw = new FileWriter(outputFolder+ "/"+ name + "_" + Thread.currentThread().getId() + ".csv", true);
-            
+
             PrintWriter out = new PrintWriter(new BufferedWriter(fw));
             out.print(toString());
 
@@ -132,7 +132,7 @@ public class Performance implements Serializable {
             e.printStackTrace();
         }
     }
-    
+
     @Override
     public String toString() {
         StringBuilder csv = new StringBuilder();
@@ -147,6 +147,63 @@ public class Performance implements Serializable {
         }
 
         return csv.toString();
+    }
+
+    public static Map<String, Long> convertRowToMap (String [] header, String []data){
+        Map <String, Long> map = new HashMap<String, Long>();
+        try{
+            if (header.length != data.length)
+                throw new IllegalArgumentException("data and header do not match");
+            for (int i = 0 ; i < header.length; i++)
+                map.put(header[i], Long.parseLong(data[i]));
+
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+
+        return map;
+    }
+
+    public static void main (String [] args) {
+        String filePath ="";
+        if (args.length > 0)
+            filePath = args[0];
+        else {
+            System.err.println("Usage: Performance <filePath>");
+            System.exit(1);
+        }
+
+        try (
+                BufferedReader br = new BufferedReader(new FileReader(filePath))
+        ){
+            String lastLine= "", tempLine = "";
+
+            // header
+            String[] header = br.readLine().split(",");
+
+            //first line
+            Map <String , Long > firstMetricRow = convertRowToMap(header, br.readLine().split(","));
+
+            while((tempLine = br.readLine())!= null){
+                lastLine = tempLine;
+            }
+
+            // last line
+            Map <String, Long > lastMetricRow = convertRowToMap(header,lastLine.split(","));
+
+            long duration = (lastMetricRow.get(header[0]) - firstMetricRow.get(header[0]))/1000;
+            long numOfRecords = lastMetricRow.get(header[1]) - firstMetricRow.get(header[1]);
+            long volume = lastMetricRow.get(header[2]) - firstMetricRow.get(header[2]);
+
+            System.out.println("Thoughput in records/s: " + numOfRecords/duration);
+            System.out.println("Thoughput in bytes/s: " + volume / duration);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
