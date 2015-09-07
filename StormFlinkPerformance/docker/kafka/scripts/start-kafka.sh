72c3278de696241690501c8f742d169561292f35
kduy <duyvkvn@gmail.com>
2015-09-08 01:23:05 +0200

    fixed: not create topics

--- StormFlinkPerformance/docker/kafka/scripts/start-kafka.sh
+++ StormFlinkPerformance/docker/kafka/scripts/start-kafka.sh
@@ -71,10 +71,10 @@ $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
 
 # create topics
 
-# if [[ ! -z $KAFKA_CREATE_TOPICS ]]; then
-#     IFS=','; for topicToCreate in $KAFKA_CREATE_TOPICS; do
-#         echo "${topicToCreate}"
-#         $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ADVERTISED_HOST:2181 --replication-factor 1 --partition 1 --topic "${topicToCreate}"
-#     done
-# fi
+if [[ ! -z $KAFKA_CREATE_TOPICS ]]; then
+    IFS=','; for topicToCreate in $KAFKA_CREATE_TOPICS; do
+        echo "${topicToCreate}"
+        $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ADVERTISED_HOST:2181 --replication-factor 1 --partition 1 --topic "${topicToCreate}"
+    done
+fi
 
